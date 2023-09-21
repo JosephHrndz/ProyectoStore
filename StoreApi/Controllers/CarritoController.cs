d6436783b13e5c5975c88eacd915de571e60e28e
ayalaalberto <ma22002@esfe.agape.edu.sv>
2023-09-20 20:51:56 -0600

    controller webApi

---
+++ StoreApi/Controllers/CarritoController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Globalization;
+using ProyectoStore.BL;
+using ProyectoStore.EN;
+using System.Text.Json;
+
+namespace StoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarritoController : Controller
+    {
+        private CarritoBL carritoBL = new CarritoBL();
+
+        [HttpGet]
+        public async Task<IEnumerable<Carrito>> Get()
+        {
+            return await carritoBL.ObtenerTodosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Carrito> Get(int id)
+        {
+            Carrito carrito = new Carrito();
+            carrito.Id = id;
+            return await carritoBL.ObtenerPorIdAsync(carrito);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Carrito carrito)
+        {
+            try
+            {
+                await carritoBL.CrearAsync(carrito);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Carrito carrito)
+        {
+            if (carrito.Id == id)
+            {
+                await carritoBL.ModificarAsync(carrito);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                Carrito carrito = new Carrito();
+                carrito.Id = id;
+                await carritoBL.EliminarAsync(carrito);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("Buscar")]
+        public async Task<List<Carrito>> Buscar([FromBody] object pCarrito)
+        {
+            var option = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            string strCarrito = JsonSerializer.Serialize(pCarrito);
+            Carrito carrito = JsonSerializer.Deserialize<Carrito>(strCarrito, option);
+            return await carritoBL.BuscarAsync(carrito);
+        }
+    }
+}
---
+++ StoreApi/Controllers/ComprarController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+
+//sssssssssssssssssssssssssssssssssssssss
+
+using ProyectoStore.BL;
+using ProyectoStore.EN;
+using System.Globalization;
+
+namespace StoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComprarController : Controller
+    {
+        private ComprarBL comprarBL = new ComprarBL();
+
+        [HttpGet]
+        public async Task<IEnumerable<Comprar>> Get()
+        {
+            return await comprarBL.ObtenerTodosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Comprar> Get(int id)
+        {
+            Comprar comprar = new Comprar();
+            comprar.Id = id;
+            return await comprarBL.ObtenerPorIdAsync(comprar);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Comprar comprar)
+        {
+            try
+            {
+                await comprarBL.CrearAsync(comprar);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Comprar comprar)
+        {
+            if (comprar.Id == id)
+            {
+                await comprarBL.ModificarAsync(comprar);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                Comprar comprar = new Comprar();
+                comprar.Id = id;
+                await comprarBL.EliminarAsync(comprar);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("Buscar")]
+        public async Task<List<Comprar>> Buscar([FromBody] object pComprar)
+        {
+            var option = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            string strComprar = JsonSerializer.Serialize(pComprar);
+            Comprar comprar = JsonSerializer.Deserialize<Comprar>(strComprar, option);
+            return await comprarBL.BuscarAsync(comprar);
+        }
+    }
+}
